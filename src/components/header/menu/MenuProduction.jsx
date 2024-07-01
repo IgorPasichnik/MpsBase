b4851d3df2ffcdb40e58d6ff80fd9b667274b0d1
IgorPasichnik <nekto.igoryambus@gmail.com>
2024-07-01 13:03:28 +0300

    menu-gray

--- src/components/header/menu/MenuProduction.jsx
+++ src/components/header/menu/MenuProduction.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 export const MenuProductions = () => {
   return (
-    <ul className="ml-[300px] mt-[4px] w-54 border-2 border-gray-200 rounded-sm flex flex-col  absolute bg-white drop-d text-customBlack">
-      <div className="p-4 border-b-2 border-gray-200 flex justify-start">
+    <ul className="ml-[300px] mt-[4px] w-54 border-t-2 border-gray-500 rounded-sm flex flex-col  absolute bg-customGray drop-d text-white">
+      <div className="p-4 border-b-2 border-gray-500 flex justify-start">
         <Link to="/productions#structures">
           <CustomButton>Металлоконструкции</CustomButton>
         </Link>
--- src/components/header/menu/MenuProducts.jsx
+++ src/components/header/menu/MenuProducts.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 export const MenuProducts = () => {
   return (
-    <ul className="ml-36 mt-[4px] w-54 border-2 border-gray-200 rounded-sm flex flex-col  absolute bg-white drop-d text-customBlack">
-      <div className="p-4 border-b-2 border-gray-200 flex justify-start">
+    <ul className="ml-36 mt-[4px] w-54 border-t-2 border-gray-500 rounded-sm flex flex-col  absolute bg-customGray drop-d text-white">
+      <div className="p-4 border-b-2 border-gray-500 flex justify-start">
         <Link to="/products#sheet">
           <CustomButton>Листовой прокат</CustomButton>
         </Link>
       </div>
-      <div className="p-4 border-b-2 border-gray-200 flex justify-start">
+      <div className="p-4 border-b-2 border-gray-500 flex justify-start">
         <Link to="/products#variety">
           <CustomButton>Сортовой прокат</CustomButton>
         </Link>
